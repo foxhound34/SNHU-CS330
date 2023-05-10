683328b4b797bd15e44a44a16b616df6b68a60a9
Derek Bamford <117669087+foxhound34@users.noreply.github.com>
2023-05-10 18:21:21 -0400

    Update main.cpp

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -14,6 +14,10 @@
 
 #include"shader.h"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include<iostream>
 
 const unsigned int WIDTH = 800;
@@ -36,7 +40,7 @@ int main()
 #endif
 
 	//Builds a window (Width, Height, Title, Full screen, <reasons>)
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Assignment 2_Bamford", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Assignment 3_Bamford", NULL, NULL);
 
 	//error checking for window
 	if (window == NULL)
@@ -58,9 +62,15 @@ int main()
 
 	GLfloat vertices[] = {
 		// positions         // colors
-		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+		 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   // top
+		-0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 
+	};
+
+	unsigned int indices[] = {
+	   0, 1, 3, // first triangle
+	   1, 2, 3  // second triangle
 	};
 
 	//The viewpoint goes from x = 0, y = 0, to x = 800, y = 600
@@ -70,9 +80,10 @@ int main()
 	// Vertex Array Object, Vertex Buffer object
 	// VAO must be generated befor the VBO
 	//In this example I am making two objects, one for each triangle
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 
 	//first triangle setup
@@ -82,12 +93,15 @@ int main()
 	//Stores vertices in VBO
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	//configures so the OpenGl knows how to use the VBO, position attributes
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	//color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
 
@@ -98,13 +112,23 @@ int main()
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+
+		// create transformations
+		glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+		transform = glm::translate(transform, glm::vec3(0.0f, 0.0f, 0.0f)); //lotion translation
+																		   //x, y, z rotation
+		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
 		ourShader.use();
+
+		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
 
 		//render the first triangle
 		glBindVertexArray(VAO);
 
 		//Drawing function start index is 0 and the number of vertices is 6, because its two triangle
-		glDrawArrays(GL_TRIANGLES, 0,3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 
@@ -115,6 +139,7 @@ int main()
 	//De-allocates resources
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwDestroyWindow(window); //Terminates the window
 	glfwTerminate(); //Terminates GLFW
