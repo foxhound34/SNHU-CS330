2b950a01d1c5f4ac10ca1269f8630f6c0c3ed29e
Derek Bamford <117669087+foxhound34@users.noreply.github.com>
2023-05-17 12:18:31 -0400

    Update main.cpp

--- Assignment 4/main.cpp
+++ Assignment 4/main.cpp
@@ -18,6 +18,10 @@
 
 #include<iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int WIDTH, int HEIGHT);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
 void processInput(GLFWwindow* window);
 
 const unsigned int WIDTH = 800;
@@ -27,6 +31,14 @@ const unsigned int HEIGHT = 600;
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
+
 
 // timing
 float deltaTime = 0.0f;	// time between current frame and last frame
@@ -57,6 +69,15 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	//Tells GLFW we would like to use the window we just created, because it is stupid
 	glfwMakeContextCurrent(window);
 
@@ -182,8 +203,8 @@ int main()
 
 	//allows OpenGl to account for the depth of the container
 	glEnable(GL_DEPTH_TEST);
-	// pass projection matrix to shader (as projection matrix rarely changes there's no need to do this per frame)
-// -----------------------------------------------------------------------------------------------------------
+
+	// pass projection matrix to shader 
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
 	ourShader.setMat4("projection", projection);
 
@@ -192,11 +213,11 @@ int main()
 	{
 
 		// per-frame time logic
-	   // --------------------
 		float currentFrame = static_cast<float>(glfwGetTime());
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
+		//Calls the keyboard commands below
 		processInput(window);
 
 		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -208,9 +229,6 @@ int main()
 		glm::mat4 model = glm::mat4(1.0f);
 		//glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 proj = glm::mat4(1.0f);
-
-		//changes the size of the object
-		//model = glm::scale(view, glm::vec3(3.0f, 3.0f, 3.0f));
 
 		//moves the world
 		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -5.0f));
@@ -238,7 +256,8 @@ int main()
 			glm::mat4 model = glm::mat4(1.0f);
 			//moves the 3D object around the world
 			model = glm::translate(model, glm::vec3(-1.5f, 0.0f, -3.0f));
-
+			//changes the size of the object
+			//model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
 			ourShader.setMat4("model", model);
 			//draws the triangles
 			glDrawElements(GL_TRIANGLES, sizeof(squareIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
@@ -251,8 +270,9 @@ int main()
 		{
 			glm::mat4 model = glm::mat4(1.0f);
 			//moves the 3D object around the world
-			model = glm::translate(model, glm::vec3(1.0f, 0.0f, -1.0f));
-
+			model = glm::translate(model, glm::vec3(0.7f, -0.3f, -2.0f));
+			//changes the size of the object
+			//model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
 			ourShader.setMat4("model", model);
 			//draws the triangles
 			glDrawElements(GL_TRIANGLES, sizeof(triangleIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
@@ -302,4 +322,61 @@ void processInput(GLFWwindow* window)
 	//controls the downward movement
 	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
 		cameraPos += cameraSpeed * -cameraUp;
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f; // change this value to your liking
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	fov -= (float)yoffset;
+	if (fov < 1.0f)
+		fov = 1.0f;
+	if (fov > 45.0f)
+		fov = 45.0f;
 }
