c12c5a0a27b4e916326d4b83d8aa550cc0686755
Derek Bamford <117669087+foxhound34@users.noreply.github.com>
2023-05-25 08:15:18 -0400

    Update main.cpp

--- Assignment 5_Bamford/main.cpp
+++ Assignment 5_Bamford/main.cpp
@@ -139,7 +139,7 @@ int main()
 
 	GLfloat tableVertices[] = {
 			// positions			 colors         texture
-		-2.0f,  -1.0f, -2.0f,  1.0f, 1.0f, 1.0f,    0.0f, 1.0f,
+		-2.0f,  -1.0f, -2.0f,  1.0f, 1.0f, 1.0f,        0.0f, 1.0f,
 		 2.0f,  -1.0f, -2.0f,  1.0f, 1.0f, 1.0f,	1.0f, 1.0f,
 		 2.0f,  -1.0f,  2.0f,  1.0f, 1.0f, 1.0f,	1.0f, 0.0f,
 		 2.0f,  -1.0f,  2.0f,  1.0f, 1.0f, 1.0f,	0.0f, 0.0f,
