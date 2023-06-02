0e36b60611b7ae625c17e4072e5b7ac6647f141f
Derek Bamford <117669087+foxhound34@users.noreply.github.com>
2023-06-01 21:37:17 -0400

    Update and rename 1.colors.fs to 2.2.basic_lighting.fs

---
+++ Assignment 6/2.2.basic_lighting.fs
@@ -0,0 +1,41 @@
+#version 330 core
+out vec4 FragColor;
+
+ 
+in vec3 Normal;  
+in vec3 FragPos; 
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+uniform vec3 lightPos; 
+uniform vec3 viewPos; 
+uniform vec3 lightColor;
+uniform vec3 objectColor;
+
+
+void main()
+{
+    //FragColor = texture(ourTexture, TexCoord);
+
+   // ambient
+    float ambientStrength = 0.1;
+
+    //Lights color multiplied strength
+    vec3 ambient = ambientStrength * lightColor;
+  	
+    // diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    
+    // specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;  
+        
+    vec3 result = (ambient + diffuse + specular) * objectColor;
+    FragColor = vec4(result, 1.0);
+}
