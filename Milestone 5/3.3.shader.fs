3a9f68646cd25916d9dcee566d38e5c611b492a4
Derek Bamford <117669087+foxhound34@users.noreply.github.com>
2023-06-03 17:55:22 -0400

    Update 3.3.shader.fs

--- Milestone 5/3.3.shader.fs
+++ Milestone 5/3.3.shader.fs
@@ -3,10 +3,14 @@ out vec4 FragColor;
   
 in vec3 ourColor;
 in vec2 TexCoord;
+uniform sampler2D texture4;
+uniform sampler2D texture6;
 
 uniform sampler2D ourTexture;
 
 void main()
 {
-    FragColor = texture(ourTexture, TexCoord);
+    //FragColor = texture(ourTexture, TexCoord);
+    // linearly interpolate between both textures (70% container, 30% caution)
+	FragColor = mix(texture(texture4, TexCoord), texture(texture6, TexCoord), 0.3);
 }
