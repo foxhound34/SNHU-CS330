c3acc42c7de0bef1303ac588bef6afa06c1e69e9
Derek Bamford <117669087+foxhound34@users.noreply.github.com>
2023-06-06 17:44:23 -0400

    Update 2.2.basic_lighting.fs

--- Assignment 6/2.2.basic_lighting.fs
+++ Assignment 6/2.2.basic_lighting.fs
@@ -15,27 +15,26 @@ uniform vec3 objectColor;
 
 void main()
 {
-    //FragColor = texture(ourTexture, TexCoord);
-
    // ambient
     float ambientStrength = 0.1;
 
-    //Lights color multiplied strength
-    vec3 ambient = ambientStrength * lightColor;
+    //Lights color multiplied by the strength
+    vec3 ambient = ambientStrength * lightColor * texture(ourTexture, TexCoord).rgb;
   	
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = diff * lightColor * texture(ourTexture, TexCoord).rgb;
     
     // specular
     float specularStrength = 0.5;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;  
+    vec3 specular = specularStrength * spec * lightColor *texture(ourTexture, TexCoord).rgb;  
         
     vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0);
+    //FragColor = texture(ourTexture, TexCoord);
 }
