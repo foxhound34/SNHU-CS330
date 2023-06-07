84c87d7aab2dbf3e34fd481c8fc1c9d4e0d408b0
Derek Bamford <117669087+foxhound34@users.noreply.github.com>
2023-06-07 13:30:28 -0400

    Update 2.2.light_cube.fs

--- Assignment 6/2.2.light_cube.fs
+++ Assignment 6/2.2.light_cube.fs
@@ -4,5 +4,5 @@ out vec4 FragColor;
 void main()
 {
     //controls color of cube lights
-    FragColor = vec4(0.678, 1.0, 0.184, 1.0); // set all 4 vector values to 1.0
+    FragColor = vec4(1.0); // set all 4 vector values to 1.0
 }
